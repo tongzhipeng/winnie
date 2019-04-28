354b46c84b9d42597de80d8a40d8672457c6ec02
tongzhipeng <tongzhipeng5688@gmail.com>
2019-04-28 22:32:09 +0800

    第一天的学习内容

---
+++ day01/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int ret = test();
+        System.out.println("ret=" + ret);
+    }
+
+    //函数function 方法method
+    public static int test() {
+        return  100;
+    }
+}
